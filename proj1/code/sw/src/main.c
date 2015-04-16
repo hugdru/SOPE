15c1e4dcadcf0376c892a09956a8099a42be6c98
Hugo Drumond <hugo.drumond@fe.up.pt>
2015-04-16 09:03:12 +0100

    sed black magic

--- proj1/code/sw/src/main.c
+++ proj1/code/sw/src/main.c
@@ -17,13 +17,13 @@
 #define PIPEREAD 0
 #define PIPEWRITE 1
 
-const char sedMagic[] = "s@([^:]*):([^:]*):(.*)@\\3 : \\1-\\2@i";
+const char sedMagic[] = "s@^(.*/){0,1}([^:.]*)[^:]*:([^:]*):(.*)$@\\4 : \\2-\\3@i";
 
 int main(int argc, char *argv[]) {
 
     if (argc != 3) {
         fprintf(stderr, "%s wordsFileName searchFileName\n", argv[0]);
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 
     struct stat stat_buf;
@@ -31,75 +31,74 @@ int main(int argc, char *argv[]) {
     if (returned != 0 || !S_ISREG(stat_buf.st_mode)) {
         errno = EINVAL;
         perror("bad search file name");
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 
     returned = stat(argv[searchFilePathIndex], &stat_buf);
     if (returned != 0 || !S_ISREG(stat_buf.st_mode)) {
         errno = EINVAL;
         perror("bad search file name");
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 
     // sw    grep ->[]- sed ->[]--(stdout)
+    /** GREP SECTION **/
+    int pipeGrepSed[2];
+    if (pipe(pipeGrepSed) < 0) {
+        perror("Could not create a pipe cut -> sw");
+        exit(EXIT_FAILURE);
+    }
 
-        /** GREP SECTION **/
-        int pipeGrepSed[2];
-        if (pipe(pipeGrepSed) < 0) {
-            perror("Could not create a pipe cut -> sw");
+    pid_t pidForkGrep = fork();
+    if (pidForkGrep < 0) {
+        perror("Failed to create a grep child process");
+        exit(EXIT_FAILURE);
+    }
+
+    switch (pidForkGrep) {
+        case -1:
+            perror("Failed to fork cut");
             exit(EXIT_FAILURE);
-        }
-
-        pid_t pidForkGrep = fork();
-        if (pidForkGrep < 0) {
-            perror("Failed to create a grep child process");
+            break;
+        case 0:
+            close(pipeGrepSed[PIPEREAD]);
+            dup2(pipeGrepSed[PIPEWRITE], STDOUT_FILENO);
+            execlp("grep", "grep", argv[searchFilePathIndex], "-i", "-n", "-H", "-o", "-f", argv[wordsFilePathIndex], NULL);
+            fprintf(stderr, "failed to exec grep\n");
             exit(EXIT_FAILURE);
-        }
+            break;
+        default:
+            // I can't close the pipeGrepCut[PIPEREAD] because the OS will see that there are
+            // no processes conected to the read end and will generate a SIGPIPE signal
+            close(pipeGrepSed[PIPEWRITE]);
+            break;
+    }
+    /** END OF GREP SECTION **/
+    /** START OF SED SECTION **/
 
-        switch (pidForkGrep) {
-            case -1:
-                perror("Failed to fork cut");
-                exit(EXIT_FAILURE);
-                break;
-            case 0:
-                close(pipeGrepSed[PIPEREAD]);
-                dup2(pipeGrepSed[PIPEWRITE], STDOUT_FILENO);
-                execlp("grep", "grep", argv[searchFilePathIndex], "-i", "-n", "-H", "-o", "-f", argv[wordsFilePathIndex], NULL);
-                fprintf(stderr, "failed to exec grep\n");
-                exit(EXIT_FAILURE);
-                break;
-            default:
-                // I can't close the pipeGrepCut[PIPEREAD] because the OS will see that there are
-                // no processes conected to the read end and will generate a SIGPIPE signal
-                close(pipeGrepSed[PIPEWRITE]);
-                break;
-        }
-        /** END OF GREP SECTION **/
-        /** START OF SED SECTION **/
+    pid_t pidForkSed = fork();
+    if (pidForkSed < 0) {
+        perror("Failed to create a cut child process");
+        exit(EXIT_FAILURE);
+    }
 
-        pid_t pidForkSed = fork();
-        if (pidForkSed < 0) {
-            perror("Failed to create a cut child process");
+    switch (pidForkSed) {
+        case -1:
+            perror("Failed to fork grep");
             exit(EXIT_FAILURE);
-        }
-
-        switch (pidForkSed) {
-            case -1:
-                perror("Failed to fork grep");
-                exit(EXIT_FAILURE);
-                break;
-            case 0:
-                dup2(pipeGrepSed[PIPEREAD], STDIN_FILENO);
-                execlp("sed", "sed", "-r", sedMagic, NULL);
-                fprintf(stderr, "failed to exec cut\n");
-                exit(EXIT_FAILURE);
-                break;
-            default:
-                close(pipeGrepSed[PIPEREAD]);
-                break;
-        }
+            break;
+        case 0:
+            dup2(pipeGrepSed[PIPEREAD], STDIN_FILENO);
+            execlp("sed", "sed", "-r", sedMagic, NULL);
+            fprintf(stderr, "failed to exec cut\n");
+            exit(EXIT_FAILURE);
+            break;
+        default:
+            close(pipeGrepSed[PIPEREAD]);
+            break;
+    }
     /** END OF SED SECTION **/
 
-    return EXIT_SUCCESS;
+    exit(EXIT_SUCCESS);
 }
 
